5518f3d3f125d0f7a8687e800f15106f91c34ffc
jasmjosa382 <jasmjosa382@ed.amdsb.ca>
2022-09-15 11:07:44 -0400

    Add project files.

---
+++ ArcadeGame/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace ArcadeGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.startButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.goLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // startButton
+            // 
+            this.startButton.BackColor = System.Drawing.Color.Transparent;
+            this.startButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.startButton.Font = new System.Drawing.Font("Microsoft YaHei UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.startButton.Location = new System.Drawing.Point(153, 147);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(177, 48);
+            this.startButton.TabIndex = 0;
+            this.startButton.Text = "Play Game";
+            this.startButton.UseVisualStyleBackColor = false;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(154, 164);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(171, 23);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Game will start in: 3";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(154, 164);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(170, 23);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Game will start in: 1";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(149, 164);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(181, 23);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Game will start in: 2";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // goLabel
+            // 
+            this.goLabel.BackColor = System.Drawing.Color.Transparent;
+            this.goLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.goLabel.ForeColor = System.Drawing.Color.White;
+            this.goLabel.Location = new System.Drawing.Point(188, 164);
+            this.goLabel.Name = "goLabel";
+            this.goLabel.Size = new System.Drawing.Size(100, 23);
+            this.goLabel.TabIndex = 5;
+            this.goLabel.Text = "Go!!!";
+            this.goLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(488, 341);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.goLabel);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Name = "Form1";
+            this.Text = "Arcade Game";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label goLabel;
+    }
+}
+
---
+++ ArcadeGame/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Media;
+
+namespace ArcadeGame
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            //change visibility of start button when clicked
+            startButton.Hide();
+            Refresh();
+            Thread.Sleep(1000);
+
+            //change to label1
+            label1.Hide();
+            Refresh();
+            Thread.Sleep(1000);
+
+            //change to label2
+            label2.Hide();
+            Refresh();
+            Thread.Sleep(1000);
+
+            //change to label3
+            label3.Hide();
+            Refresh();
+            Thread.Sleep(50);
+
+            //Change backcolour
+            this.BackColor = Color.GreenYellow;
+            Refresh();
+            Thread.Sleep(500);
+
+            //create a sound player and load the alert.wav sound, then play it 
+            SoundPlayer alertPlayer = new SoundPlayer(Properties.Resources.gameBegin);
+            alertPlayer.Play();
+            Refresh();
+            Thread.Sleep(1000);
+
+            //Change colour of form1 and to Go
+            goLabel.Hide();
+            
+           
+
+            
+
+            
+
+
+            
+
+            
+
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+    }
+    
+
